117d2483331416bc28dc8d60602bffb5feff40cf
Lakhvinder Dhanoya <dhanoyal@live.com>
2022-08-01 18:08:22 -0700

    did nothing

--- ghi/app/src/index.js
+++ ghi/app/src/index.js
@@ -3,8 +3,34 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+
+async function loadInventory() {
+  let vehicleData
+  const vehicleResponse = await fetch('http://localhost:8100/api/automobiles/');
+  // const hatResponse = await fetch('http://localhost:8090/api/hats/');
+
+  if (vehicleResponse.ok) {
+    vehicleData = await vehicleResponse.json();
+    console.log('vehicle data: ', vehicleData)
+  } else {
+    console.error(vehicleResponse);
+  }
+  // if (hatResponse.ok) {
+  //   hatData = await hatResponse.json();
+  //   console.log('hat data: ', hatData)
+  // } else {
+  //   console.error(hatResponse);
+  // }
+
+  root.render(
+    <React.StrictMode>
+      <App vehicles={vehicleData} />
+    </React.StrictMode>
+  );
+}
+loadInventory()
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
