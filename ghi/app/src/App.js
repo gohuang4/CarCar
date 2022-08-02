887107f5636dec5d8ce00618f80c146dad37b8c6
Gordon <gohuang4@gmail.com>
2022-08-01 18:00:29 -0700

    test

--- ghi/app/src/App.js
+++ ghi/app/src/App.js
@@ -1,14 +1,20 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainPage from './MainPage';
+import ManufacturerList from './ManufacturersList';
+import ManufacturerForm from './ManufacturerForm';
 import Nav from './Nav';
 
-function App() {
+function App(props) {
   return (
     <BrowserRouter>
       <Nav />
       <div className="container">
         <Routes>
           <Route path="/" element={<MainPage />} />
+          <Route path="manufacturers">
+            <Route index element={<ManufacturerList manufacturers={props.manufacturers} />}/>
+            <Route path="new" element={<ManufacturerForm />} />
+          </Route>
         </Routes>
       </div>
     </BrowserRouter>
--- ghi/app/src/index.js
+++ ghi/app/src/index.js
@@ -8,3 +8,37 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import App from './App';
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+
+
+// async function loadInventory() {
+//   let serviceData, saleData;
+//   const serviceResponse = await fetch('http://localhost:8080/api/services/');
+//   const saleResponse = await fetch('http://localhost:8090/api/sales/');
+
+//   if (serviceResponse.ok) {
+//     serviceData = await serviceResponse.json();
+//     console.log('service data: ', serviceData)
+//   } else {
+//     console.error(serviceResponse);
+//   }
+//   if (saleResponse.ok) {
+//     saleData = await saleResponse.json();
+//     console.log('sale data: ', saleData)
+//   } else {
+//     console.error(saleResponse);
+//   }
+
+//   root.render(
+//     <React.StrictMode>
+//       <App service={serviceData} sales={saleData} />
+//     </React.StrictMode>
+//   );
+// }
+// loadInventory();
